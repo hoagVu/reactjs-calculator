320d94173ac86dc06b98556e04989bf9a212e5a9
mikhail-kalashnikov <hoangk62uet@gmail.com>
2020-07-09 23:21:51 +0700

    add button & input handler

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,40 +1,57 @@
 import React, { Component } from 'react';
 import './App.css';
 import Button from '../components/Button/Button';
+import Input from '../components/Input/Input';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      previousNumber: '',
+      currentNumber: '',
+      operator: '',
+    };
+  }
+  addNumberToInput = (val) => {
+    const addedNumber = this.state.input + val;
+    this.setState({ input: addedNumber });
+  };
   render() {
     return (
       <div className="App">
         <div className="calc-main">
           <div className="row">
-            <Button>AC</Button>
-            <Button>#</Button>
-            <Button>%</Button>
-            <Button>/</Button>
+            <Input></Input>
           </div>
           <div className="row">
-            <Button>7</Button>
+            <Button>AC</Button>
+            <Button>#</Button>
+            <Button className="operator">%</Button>
+            <Button className="operator">/</Button>
+          </div>
+          <div className="row">
+            <Button click=={this.addNumberToInput}>7</Button>
             <Button>8</Button>
             <Button>9</Button>
-            <Button>*</Button>
+            <Button className="operator">*</Button>
           </div>
           <div className="row">
             <Button>4</Button>
             <Button>5</Button>
             <Button>6</Button>
-            <Button>-</Button>
+            <Button className="operator">-</Button>
           </div>
           <div className="row">
             <Button>1</Button>
             <Button>2</Button>
             <Button>3</Button>
-            <Button>+</Button>
+            <Button className="operator">+</Button>
           </div>
           <div className="row">
-            <Button>0</Button>
             <Button></Button>
+            <Button>0</Button>
             <Button>.</Button>
-            <Button>=</Button>
+            <Button className="operator">=</Button>
           </div>
         </div>
       </div>
