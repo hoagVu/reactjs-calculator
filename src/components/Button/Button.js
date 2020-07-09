c27734ed84519d14d01abc72d8fe1e50c18994c1
mikhail-kalashnikov <hoangk62uet@gmail.com>
2020-07-09 23:22:57 +0700

    add handler

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Button.css';
-
-class Button extends Component {
-  render() {
-    return <div className="button">{this.props.children}</div>;
-  }
-}
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const operator = ['.', '='];
+const isOperator = (val) => {
+  return !isNaN(val) || val === '.' || val === '=' || val === null;
+};
+const Button = (props) => {
+  return (
+    <div className={`button ${isOperator(props.children) ? null : 'operator'}`}>
+      {props.children}
+    </div>
+  );
+};
 
 export default Button;
---
+++ src/components/Input/Input.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Input.css';
+const Input = (props) => {
+  return <div className="input">{props.children}</div>;
+};
+export default Input;
