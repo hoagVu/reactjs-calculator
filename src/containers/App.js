c9b998b8e168bb374bb13152a3846e1ac3c8ef2c
mikhail-kalashnikov <hoangk62uet@gmail.com>
2020-07-09 23:55:04 +0700

    add Zero Decimal && clear Input

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,42 +16,59 @@ class App extends Component {
     const addedNumber = this.state.input + val;
     this.setState({ input: addedNumber });
   };
+  addZero = (val) => {
+    if (this.state.input !== '') {
+      // add 0 when this,state,input is't empty
+      const addedNumber = this.state.input + val;
+      this.setState({ input: addedNumber });
+    }
+  };
+  addDecimal = (val) => {
+    const inputString = this.state.input;
+    if (inputString !== '' && inputString.indexOf('.') === -1) {
+      const addedNumber = this.state.input + val;
+      this.setState({ input: addedNumber });
+    }
+  };
+  clearInput = () => {
+    this.setState({ input: '' });
+  };
   render() {
     return (
       <div className="App">
         <div className="calc-main">
           <div className="row">
-            <Input></Input>
+            <Input>{this.state.input}</Input>
           </div>
           <div className="row">
-            <Button>AC</Button>
-            <Button>#</Button>
-            <Button className="operator">%</Button>
-            <Button className="operator">/</Button>
+            <Button clicked={this.clearInput}>AC</Button>
+            <Button>-Var</Button>
+            <Button>%</Button>
+            <Button>/</Button>
           </div>
           <div className="row">
-            <Button click=={this.addNumberToInput}>7</Button>
-            <Button>8</Button>
-            <Button>9</Button>
-            <Button className="operator">*</Button>
+            <Button clicked={this.addNumberToInput}>7</Button>
+            <Button clicked={this.addNumberToInput}>8</Button>
+            <Button clicked={this.addNumberToInput}>9</Button>
+            <Button>*</Button>
           </div>
           <div className="row">
-            <Button>4</Button>
-            <Button>5</Button>
-            <Button>6</Button>
-            <Button className="operator">-</Button>
+            <Button clicked={this.addNumberToInput}>4</Button>
+            <Button clicked={this.addNumberToInput}>5</Button>
+            <Button clicked={this.addNumberToInput}>6</Button>
+            <Button>-</Button>
           </div>
           <div className="row">
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
-            <Button className="operator">+</Button>
+            <Button clicked={this.addNumberToInput}>1</Button>
+            <Button clicked={this.addNumberToInput}>2</Button>
+            <Button clicked={this.addNumberToInput}>3</Button>
+            <Button>+</Button>
           </div>
           <div className="row">
             <Button></Button>
-            <Button>0</Button>
-            <Button>.</Button>
-            <Button className="operator">=</Button>
+            <Button clicked={this.addZero}>0</Button>
+            <Button clicked={this.addDecimal}>.</Button>
+            <Button>=</Button>
           </div>
         </div>
       </div>
