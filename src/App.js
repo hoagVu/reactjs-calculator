e47f1439452e5b7857b9628d5b0bd2a1bf79ce16
mikhail-kalashnikov <hoangk62uet@gmail.com>
2020-07-09 11:03:42 +0700

    use class component and create state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './components/Person/Person.js';
-function App() {
-  return (
-    <div className="App">
-      <h1> Hello World!</h1>
-      <p>This is really working!</p>
-      <Person name="Hoang" age="28"></Person>
-      <Person name="Vu" age="29">
-        My Hobbies: Coding
-      </Person>
-      <Person name="Viet" age="20"></Person>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Hoang', age: '29' },
+      { name: 'Vu', age: '25' },
+      { name: 'Viet', age: '24' },
+    ],
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1> Hello World!</h1>
+        <p>This is really working!</p>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        ></Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My Hobbies: Coding
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        ></Person>
+      </div>
+    );
+  }
 }
 
 export default App;
